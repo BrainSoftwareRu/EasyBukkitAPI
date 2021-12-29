474e5f794070439a4f0906ce2afaaf56aa16b48b
BrainSoftwareRu <92357269+BrainSoftwareRu@users.noreply.github.com>
2021-12-29 14:07:47 +0500

    Create EasyBukkit.java

---
+++ Project/ru/brainsoft/easybukkit/EasyBukkit.java
@@ -0,0 +1,10 @@
+package ru.brainsoft.easybukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EasyBukkit extends JavaPlugin {
+		
+	public void onEnable() {
+		Console.Info("Plugin is Enable!");
+	}
+}
